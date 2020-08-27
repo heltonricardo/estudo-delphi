434892e80eb693ffb3593bddcb68f2c16e828827
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-27 17:02:41 -0300

    Create Project1.dpr

---
+++ Tipos de Variáveis/Project1.dpr
@@ -0,0 +1,21 @@
+program Project1;
+
+var
+  // Integers:
+  b : byte;       // 0 .. 255
+  w : word;			  // 0 .. 65535
+  c : cardinal;   // ou longWord	0 .. 4294967295
+  shi : shortInt;	// -128 .. 127
+  smi : smallInt;	// –32768 .. 32767
+  i : integer;    // ou longInt	–2147483648 .. 2147483647
+  i64 : int64;		// –2^63 .. 2^63–1
+
+  // Floating Point:
+  s : single;		  // 1.5 x 10^–45 .. 3.4 x 10^38
+  r : real;       // ou double 5.0 x 10^–324 .. 1.7 x 10^308
+  e : extended;	  // 3.6 x 10^–4951 .. 1.1 x 10^4932
+
+begin
+  // Para atribuir um valor a uma variável é usado :=, por exemplo:
+  i := 1;
+end.
