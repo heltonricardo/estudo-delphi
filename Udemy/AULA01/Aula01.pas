3f27e89051beff86a25b440d0480f676f59c300c
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-04 14:30:11 -0300

    Update Aula01.pas

--- Udemy/AULA01/Aula01.pas
+++ Udemy/AULA01/Aula01.pas
@@ -4,13 +4,18 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Touch.Keyboard;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Touch.Keyboard,
+  Vcl.ExtCtrls;
 
 type
   TForm1 = class(TForm)
     Edit1: TEdit;
     Button1: TButton;
     TouchKeyboard1: TTouchKeyboard;
+    Panel1: TPanel;
+    Button2: TButton;
+    Panel2: TPanel;
+    Button3: TButton;
   private
     { Private declarations }
   public
